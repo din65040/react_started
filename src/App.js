f641ff531d079464b6f051c20f16f8604a076c79
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-05-29 11:39:58 +0900

    feat；リスト 3-22 サンプル作成

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import React, { Component } from "react";
-import Rect from './components/Rect';
+// import Rect from "./components/Rect";
 import "./App.css";
 
 class App extends Component {
+  msgStyle = {
+    fontSize: "24px",
+    color: "#900",
+    margin: "20px 0",
+    padding: "5px",
+    borderBottom: "2px solid #900",
+  };
+
+  msgStyle2 = {
+    fontSize: "20px",
+    color: "white",
+    backgroundColor: "#900",
+    margin: "20px 0",
+    padding: "5px",
+    borderBottom: "2px solid #900",
+  };
+
+  btnStyle = {
+    fontSize: "16px",
+    padding: "0 10px",
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      msg: "count start!",
+      flg: true,
+    };
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction() {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+      msg: "count" + state.counter,
+      flg: !state.flg,
+    }));
+  }
+
   render() {
     return (
       <div>
         <h1>React</h1>
-        <Rect x="50" y="50" w="150" h="150" c="cyan" r="50"/>
-        <Rect x="150" y="100" w="150" h="150" c="magenta" r="75"/>
-        <Rect x="100" y="150" w="150" h="150" c="black" r="25"/>
+        {this.state.flg ? (
+          <p style={this.msgStyle1}>count: {this.state.msg}</p>
+        ) : (
+          <p style={this.msgStyle2}>count: {this.state.msg}</p>
+        )}
+        <button style={this.btnStyle} onClick={this.doAction}>
+          Click
+        </button>
       </div>
     );
   }
 }
+
 export default App;
