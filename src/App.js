d464bbfb3ada8c0b1e4ac1c2d29c110894bcf7f0
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-05-29 12:15:33 +0900

    feat：リスト 3-23 サンプル作成

--- src/App.js
+++ src/App.js
@@ -3,60 +3,59 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+
+  data = [];
+
   msgStyle = {
-    fontSize: "24px",
+    fontSize: "20px",
     color: "#900",
     margin: "20px 0",
-    padding: "5px",
-    borderBottom: "2px solid #900",
-  };
+    padding: "5px"
+  }
 
-  msgStyle2 = {
-    fontSize: "20px",
-    color: "white",
-    backgroundColor: "#900",
-    margin: "20px 0",
-    padding: "5px",
-    borderBottom: "2px solid #900",
-  };
-
-  btnStyle = {
-    fontSize: "16px",
-    padding: "0 10px",
-  };
+  area = {
+    width: "500px",
+    height: "500px",
+    border: "1px solid blue"
+  }
 
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
-      msg: "count start!",
-      flg: true,
+      list: this.data
     };
     this.doAction = this.doAction.bind(this);
   }
 
-  doAction() {
-    this.setState((state) => ({
-      counter: state.counter + 1,
-      msg: "count" + state.counter,
-      flg: !state.flg,
-    }));
+  doAction(e) {
+    let x = e.pageX;
+    let y = e.pageY;
+    this.data.push({x, y})
+    this.setState({
+      list: this.data
+    })
+  }
+
+  draw(d) {
+    let s = {
+      position: "absolute",
+      left: (d.x) + "px",
+      top: (d.y) + "px",
+      width: "50px",
+      height: "50px",
+      backgroundColor: "#66f3",
+    };
+    return <div style={s}></div>
   }
 
   render() {
-    return (
-      <div>
-        <h1>React</h1>
-        {this.state.flg ? (
-          <p style={this.msgStyle1}>count: {this.state.msg}</p>
-        ) : (
-          <p style={this.msgStyle2}>count: {this.state.msg}</p>
-        )}
-        <button style={this.btnStyle} onClick={this.doAction}>
-          Click
-        </button>
+    return <div>
+      <h1>React</h1>
+      <h2 style={this.msgStyle}>show rect.</h2>
+      <div style={this.area} onClick={this.doAction}>
+        {this.data.map((value)=>this.draw(value))}
       </div>
-    );
+    </div>
   }
 }
 
